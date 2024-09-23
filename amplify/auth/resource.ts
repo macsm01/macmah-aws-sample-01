c1e56a6a7b63ad1c17b064e6777729841fd29195
macsm01 <113207619+macsm01@users.noreply.github.com>
2024-09-24 00:30:33 +0100

    Update resource.ts

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,11 +1,23 @@
-import { defineAuth } from "@aws-amplify/backend";
+import {defineAuth, secret} from "@aws-amplify/backend";
+import 'dotenv/config';
 
-/**
- * Define and configure your auth resource
- * @see https://docs.amplify.aws/gen2/build-a-backend/auth
- */
 export const auth = defineAuth({
   loginWith: {
-    email: true,
+    email: {
+      verificationEmailSubject: 'Welcome to the Blog post app 👋 Verify your email!'
+    },
+    externalProviders: {
+      oidc: [
+        {
+          name: 'Auth0',
+          clientId: secret('AUTH0_CLIENT_ID'),
+          clientSecret: secret('AUTH0_CLIENT_SECRET'),
+          issuerUrl: process.env.ISSUER_URL || '',
+          scopes: ['openid', 'profile', 'email']
+        },
+      ],
+      callbackUrls: [process.env.CALLBACK_URL || ''],
+      logoutUrls: [process.env.LOGOUT_URL || ''],
+    },
   },
 });
